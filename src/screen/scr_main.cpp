3f4191682a04017f552894c496316882875d310a
Devon Jacobson <dev2708dev@gmail.com>
2019-07-30 10:08:33 +0200

    Exercise 4.1

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -7,26 +7,37 @@ using namespace std;
 int main()
 {
 	auto myScreen = Screen{6,6};
+	
+	
+	
+	for(int i = 0; i < 6; i++){
+		myScreen.set('*');
+		if(i != 5) myScreen.down();
+	}
+	
+	myScreen.move(1,2);
+	myScreen.set('*');
+	myScreen.forward();
+	myScreen.set('*');
+	myScreen.down();
+	myScreen.forward();
+	myScreen.set('*');
+	myScreen.down();
 	myScreen.forward();
 	myScreen.set('*');
 	myScreen.down();
 	myScreen.set('*');
-	myScreen.move(3,3);
-	myScreen.set("---");
-
+	myScreen.back();
+	myScreen.down();
+	myScreen.set('*');
+	myScreen.back();
+	myScreen.down();
+	myScreen.set('*');
+	myScreen.back();
+	myScreen.set('*');
+	
 	myScreen.display();
-	cout << endl;
-
-	myScreen.reSize(16,16);
-	myScreen.display();
-	myScreen.clear(' ');
-
-	myScreen.move(7,7);
-	myScreen.set("BIG");
-	myScreen.move(8,5);
-	myScreen.set("SCREEN");
-	myScreen.display();
-
+	
 	return 0;
 }
 
