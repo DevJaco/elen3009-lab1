b9c3710644c36ab8d3c7c8ad49f6613a3f99ca76
Devon Jacobson <dev2708dev@gmail.com>
2019-07-30 09:39:48 +0200

    Exercise 3.3

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -11,18 +11,31 @@ using namespace std; // cout, endl, complex are all part of this namespace
 int main()
 {
 	using comp = complex<float>;
-	auto num1 = comp{2.0, 2.0};  // use auto for type deduction
-	auto num2 = comp{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
+	int a, b, c; // use uniform initialisation syntax (curly braces)
+	bool again = false;
+do {
+	printf ("Enter the value for a: ");
+	scanf ("%d",&a);
+	printf ("Enter the value for b: ");
+	scanf ("%d",&b);
+	printf ("Enter the value for c: ");
+    scanf ("%d",&c);
+	
+	
 
-	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
+	comp x1 = (float(-b) +  sqrt(std::complex<float>(b*b - 4*a*c, 0))) / float(2*a); // type deduced for 'answer' is: complex<float> if delta is negative
+	comp x2 = (float(-b) - sqrt(std::complex<float>(b*b - 4*a*c, 0))) / float(2*a); // type deduced for 'answer' is: complex<float> if delta is negative
 
-	cout << "The answer is: " << answer << endl;
-	cout << "Or in a more familiar form: " << answer.real() 
-			<< " + " << answer.imag() << "j" 
-			<< endl	<< endl;
+	cout << "The first root is: " << x1.real() << " + " << x1.imag() << "j"<< endl;
+	cout << "The second root is: " <<  x2.real() << " + " << x2.imag() << "j"<< endl;;
 
 	//answer++; 
 	//This doesn't work because the postfix ++ operator is not defined for the complex type
-
+	
+	printf ("Do you want to do another calculation? Enter 1 for yes and 0 for no ");
+    scanf ("%d",&again);
+}
+while(again);
 	return 0;
 }
+
