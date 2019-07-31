c1ab1d1102fd8774d1b9f1ff3e8194c8ac4d2d56
Kushail <kushailsingh98@gmail.com>
2019-07-31 20:57:25 +0200

    Exercise 4.6

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -13,5 +13,17 @@ int main()
 	myScreen.emptySquare(2,2,3);
     myScreen.display();
 	return 0;
+    
 }
 
+/* Exercise 4.6
+A 2D array is a more intuitive internal representation that can be used 
+without having to change the existing public interface. It is more 
+more understandable to think of a grid-screen as a 2 dimensional array.
+
+This would not change the class interface only the class implementation.
+It is important not to change the class interface as all existing clients
+of the class would be impacted if the interface is changed. However, we 
+are free to change the implementation as long as function parameters 
+remain the same. 
+*/ 
